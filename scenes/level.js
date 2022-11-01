506d4ef5957115cf85a77ab6dea61fa92aa30f36
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-11-01 17:45:57 -0400

    Building generation script (upside-down)

--- scenes/level.js
+++ scenes/level.js
@@ -5,10 +5,38 @@ class Level extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image("world1brick", "assets/image/world1brick.png");
     }
 
     create() {
+        var levelStats = {
+            stdBuildingWidth: 10,
+            buildingWidthDev: 3,
+            numBuildings: 12,
+            stdGapWidth: 3,
+            gapWidthDev: 1,
+            stdBuildingHeight: 5,
+            buildingHeightDev: 2,
+        };
+        
+        this.bricks = this.physics.add.group();
+        var buildingStartPos = 2;
+        for (var i = 0; i < levelStats.numBuildings; i++) {
+            var buildingWidth = levelStats.stdBuildingWidth + (2 * Math.random() - 1) * levelStats.buildingWidthDev;
+            var buildingHeight = levelStats.stdBuildingHeight + (2 * Math.random() - 1) * levelStats.buildingHeightDev;
+            var gapWidth = levelStats.stdGapWidth + (2 * Math.random() - 1) * levelStats.gapWidthDev;
+            
+            for (var col = 0; col < buildingWidth; col++) {
+                for (var row = 0; row < buildingHeight; row++) {
+                    var brick = this.bricks.create((buildingStartPos + col) * 32, row * 32, "world1brick");
+                    brick.setScale(32 / 256);
+                }
+            }
+            
+            buildingStartPos += buildingWidth + gapWidth;
+        }
+        
+        
         
     }
 
