cabc691094381e86e802422ce46b02cf51fb519d
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-11-05 17:03:31 -0400

    Complete transition scene (MVP)

--- scenes/transition.js
+++ scenes/transition.js
@@ -17,6 +17,12 @@ PROPERTY DAMAGE: $${game.scoreStats.propertyDamage}.00
                 lineSpacing: -5,
             }
         );
+        this.okButton = this.add.text(100, 500, "CONTINUE");
+        this.okButton.setInteractive();
+        this.okButton.on("pointerdown", function() {
+            game.levelOn++;
+            this.scene.start("level");
+        }.bind(this));
     }
 
     update() {
