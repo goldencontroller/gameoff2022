27f92d8f156c870f05a7088b95990732dccd610b
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-11-26 10:52:47 -0500

    Update credits.js

--- scenes/credits.js
+++ scenes/credits.js
@@ -19,7 +19,14 @@ class Credits extends Phaser.Scene {
         this.closeButton.on("pointerover", function() { this.setColor("grey"); });
         this.closeButton.on("pointerout", function() { this.setColor("white"); });
         this.mainText = this.add.text(100, 100, `
-Created from head to toe by Yikuan Sun.
+Designed by Yikuan Sun
+Developed by Yikuan Sun
+Graphics created by Yikuan Sun
+Sound effects created by Yikuan Sun
+Music written by Yikuan Sun
+Based on a story by Yikuan Sun
+The MAtri;x
+
 All characters and plots are fictional and do not relate to real people or events.`);
     }
 
